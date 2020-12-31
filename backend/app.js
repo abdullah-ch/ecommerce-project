177317c75e2e5cc782db22331fa65b873ed5f74f
abdullah-ch <abdullahchaudharry@gmail.com>
2020-12-31 17:09:02 +0500

    established express server

---
+++ backend/app.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+// connecting to MongoDB
+const mongoURL = process.env.MONGO_URL;
+
+mongoose
+  .connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("DataBase has been connected !"))
+  .catch((err) => console.log("Cannot connect to database", err.message));
+
+
+app.get("/", (req,res) => {
+    res.send(`Hello`)
+})
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`I am listening at ${port}`);
+});
