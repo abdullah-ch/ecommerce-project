c2a4ae8cce644357b29dc3ab9bf41db54dbc5ed3
abdullah-ch <abdullahchaudharry@gmail.com>
2021-01-05 03:22:26 +0500

    added JWT Authentication

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const { Users, userSchema } = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
@@ -16,15 +17,28 @@ router.post("/register", async (req, res) => {
     if (password.length < 6)
       return res.status(404).json({ msg: "The password is less than 6" });
 
+    // Hashing password
     const newPassword = await bcrypt.hash(password, 10);
 
+    // creating a user and saving in db
     user = await new Users({ name, email, password: newPassword });
     await user.save();
 
-    return res.json({ password, newPassword, user });
+    // creating JWT token for authenticating the user
+    const accessToken = createAccessToken({ id: user._id });
+    const refreshToken = createRefreshToken({ id: user._id });
+
+    return res.json({ password, newPassword, user, accessToken, refreshToken });
   } catch (err) {
     return res.status(500).json({ msg: err.message });
   }
 });
 
+const createAccessToken = (userID) => {
+  return jwt.sign(userID, process.env.ACCESS_TOKEN, { expiresIn: "1 days" });
+};
+
+const createRefreshToken = (userID) => {
+  return jwt.sign(userID, process.env.ACCESS_TOKEN, { expiresIn: "7 days" });
+};
 module.exports = router;
