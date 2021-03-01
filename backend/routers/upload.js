233eb0b4f25b71dec36d89cb280a781211047cc3
abdullah-ch <abdullahchaudharry@gmail.com>
2021-03-01 22:43:52 +0500

    added image upload functionality

--- backend/routers/upload.js
+++ backend/routers/upload.js
@@ -11,8 +11,69 @@ cloudinary.config({
   api_secret: process.env.CLOUD_API_SECRET,
 });
 
-router.post("/upload", auth, authAdmin, async (req, res) => {});
+// auth,  authAdmin,
+// brooo yeh yaad sae daal loo
+router.post("/upload", (req, res) => {
+  try {
+    console.log("Object keys", Object.keys(req.files).length);
+    if (!req.files || Object.keys(req.files).lenght === 0)
+      return res.status(400).json({ msg: "No image was uploaded" });
+    const { file } = req.files;
 
-router.delete("/delete/:id", auth, authAdmin, async (req, res) => {});
+    if (file.size > 1048576) {
+      removeTempFiles(file.tempFilePath);
+      return res.status(400).json({ msg: "Image size is larger than 1mb " });
+    }
+
+    console.log("my file type is", file.mimetype);
+    if (
+      file.mimetype != "image/jpg" &&
+      file.mimetype != "image/png" &&
+      file.mimetype != "image/jpeg"
+    ) {
+      removeTempFiles(file.tempFilePath);
+      return res.json({ msg: "Format not supported" });
+    }
+
+    cloudinary.v2.uploader.upload(
+      file.tempFilePath,
+      { folder: "test" },
+      async (err, result) => {
+        if (err) throw err;
+
+        removeTempFiles(file.tempFilePath);
+
+        return res.json({
+          public_id: result.public_id,
+          url: result.secure_url,
+        });
+      }
+    );
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
+});
+
+router.post("/delete", async (req, res) => {
+  try {
+    const { public_id } = req.body;
+    if (!public_id)
+      return res.status(400).json({ msg: "Please Select an image" });
+    cloudinary.v2.uploader.destroy(public_id, async (err, result) => {
+      if (err) throw err;
+      return res.json({ msg: "Image Deleted" });
+    });
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
+});
+
+const removeTempFiles = (path) => {
+  fs.unlink(path, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+};
 
 module.exports = router;
