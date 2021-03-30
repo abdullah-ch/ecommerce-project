4fb2553ab835c5d753967d5a88bbb5e71c9850b0
abdullah-ch <abdullahchaudharry@gmail.com>
2021-03-30 05:11:37 +0500

    added assets

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -1,9 +1,38 @@
 import React, { useContext } from "react";
 import { GlobalState } from "../GlobalState/GlobalState";
+import Bars from "../../Assets/bars-solid.svg";
+import Cart from "../../Assets/shopping-cart-solid.svg";
+import Close from "../../Assets/times-solid.svg";
+import "./Header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const value = useContext(GlobalState);
-  return <div>{value}</div>;
+  return (
+    <header>
+      <div className="menu">
+        <img src={Bars} alt="menu bars" width="30px" />
+      </div>
+
+      <div className="logo">
+        <h1>
+          <Link to="/"> Berserk Logo Here</Link>
+        </h1>
+      </div>
+
+      <ul>
+        <li>
+          <Link to="/"> Products</Link>
+        </li>
+        <li>
+          <Link to="/"> Login - Register </Link>
+        </li>
+        <li>
+          <img src={Close} alt="close" width="30px" />
+        </li>
+      </ul>
+    </header>
+  );
 };
 
 export default Header;
